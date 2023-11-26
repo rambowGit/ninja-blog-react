25bcf60b153c0d1ef209b53746a5bb3ebdf81303
Stepkin Aleksandr Yurevich <stepkin@speechpro.com>
2023-11-26 15:24:44 +0300

    react-router-dom added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 
 const Home = () => {
-  
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-  
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('could not fetch the data for that resource');
-      }
-      return res.json();
-    })
-    .then((data) => {
-      setBlogs(data);
-      setIsPending(false);
-      setError(null);
-    })
-    .catch(err => {
-      setIsPending(false);
-      console.error('** Data fetching error: ', err.message)
-      setError(err.message);
-    });
-  }, []);
+
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
   
   return ( 
     <div className="home">
